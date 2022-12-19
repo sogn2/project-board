131a2843a42053447f3c9ae7146e525f3185843f
song0412 <sognrok1@gmail.com>
2022-12-19 21:48:54 +0900

    #12 임시

--- src/main/java/com/bitstudy/app/domain/Ex00_1_Article.java
+++ src/main/java/com/bitstudy/app/domain/Ex00_1_Article.java
@@ -1,19 +1,8 @@
 package com.bitstudy.app.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-import javax.persistence.Entity;
 import java.time.LocalDateTime;
-/*롬복을 이용해서 클래스를 엔티티로 변경
-* get/setter,toString등의 롬복 어노테이션 사용
-* 동등성, 동일성 비교할 수 잇는 코드 넣어볼거임*/
-@Getter
-@Setter
-@ToString
-@Entity
-public class Article {
+
+public class Ex00_1_Article {
     private Long id;
     private String title;
     private String content;
