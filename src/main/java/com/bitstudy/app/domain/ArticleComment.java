8837b95cfeabe21249174905b941b3059714e63a
song0412 <sognrok1@gmail.com>
2022-12-19 19:14:04 +0900

    #7 - 도메인 클래스 설계(게시글, 댓글)
    도메인 정보 설계 했음. (Article, AriticleComment)
    기초적인 게시글과 댓글 표현이 가능하도록 함.
    DB와 연동을 위한 추가적인 표현은 추후 업데이트 할 예정.

---
+++ src/main/java/com/bitstudy/app/domain/ArticleComment.java
@@ -0,0 +1,18 @@
+package com.bitstudy.app.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long id;
+
+    private Article article;
+    private String content;
+
+
+    //메타데이터
+    private LocalDateTime createAt;//생성일시
+    private String createBy;//생성자
+    private LocalDateTime modifiedAt;//수정일시
+    private String modifiedBy;//수정자
+
+}
