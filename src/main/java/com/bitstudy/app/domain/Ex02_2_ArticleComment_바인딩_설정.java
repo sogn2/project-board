bddf5af16cf14affd0e9e084da60f9d2f45c7eab
song0412 <sognrok1@gmail.com>
2022-12-22 20:58:09 +0900

    #12 - 도메인 코드 엔티티로 변환
    1 도메인 클래스 = 1 테이블로 매핑.
    생성자, 'equals()', 'hashcode()'등 설정

---
+++ src/main/java/com/bitstudy/app/domain/Ex02_2_ArticleComment_바인딩_설정.java
@@ -0,0 +1,51 @@
+package com.bitstudy.app.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(indexes = {
+        @Index(columnList = "content"),//name 생략
+        @Index(columnList = "createAt"),
+        @Index(columnList = "createBy"),
+})
+public class Ex02_2_ArticleComment_바인딩_설정 {
+    @Id     //pk키
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //autoincrement 1씩 자동증가
+    private Long id;
+
+   @Setter
+   @ManyToOne(optional = false)
+
+   private Article article;
+    @Setter
+    @Column(nullable = false,length = 500)
+    String content;
+
+
+    //메타데이터
+    @CreatedDate
+    @Column(nullable=false)
+    private LocalDateTime createAt;//생성일시
+
+    @CreatedBy
+    @Column(nullable=false,length = 100)
+    private String createBy;//생성자
+
+    @LastModifiedDate
+    @Column(nullable=false)
+    private LocalDateTime modifiedAt;//수정일시
+
+    @LastModifiedBy
+    @Column(nullable=false,length = 100)
+    private String modifiedBy;//수정자
+
+}
