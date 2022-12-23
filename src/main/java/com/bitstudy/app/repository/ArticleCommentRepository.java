e34bcaeacc9cef613bc788d9d56892ae04e9ad4a
song0412 <sognrok1@gmail.com>
2022-12-23 21:48:47 +0900

    #5 - 슬라이스 테스트(실패용)
    실패용 예제 테스트 파일.
    슬라이스트 테스트로 시도 했지만 Controller 외의 bean들을 로드하지 못해서 실패

--- src/main/java/com/bitstudy/app/repository/ArticleCommentRepository.java
+++ src/main/java/com/bitstudy/app/repository/ArticleCommentRepository.java
@@ -3,5 +3,11 @@ package com.bitstudy.app.repository;
 import com.bitstudy.app.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+/*TDD를 위해서 임시로 만들어놓은 저장소 (이걸로 DB 접근)
+ * 할일 - 클래스 레벨에 @RepositoryRestResource 넣어서 해당 클래스를 spring rest
+ * */
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
---
+++ src/test/java/com/bitstudy/app/controller/DataRestTest.java
@@ -0,0 +1,48 @@
+package com.bitstudy.app.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/*
+*   슬라이스 테스트 : 기능(레이어)별로 잘라서 특정 부문(기능)만 테스트 하는 것
+* 
+*   - 통합테스트 에너테이션
+*       @SpringBootTest - 스프링이 관리하는 모든 빈을 등록시켜서 테스트하기 때문에 무겁다
+*                       * 테스트할 가볍게 하기 위해서 @WebMvcTest를 사용해서 web 레이어 관련 빈들만 등록한 상태로 테스트를 할 수도 있다.
+*                          단, web 레이어 관련된 빈들만 등록되므로 Service는 등록되지 않는다. 그래서 Mock 관련 언노테이션을 이용해서 가짜로 만들어줘야 한다.
+*    - 슬라이스테스트 에너테이션
+* 1) @WebMvcTest - 슬라이스 테스트에서 대표적인 어노테이션
+* */
+@WebMvcTest
+public class DataRestTest {
+    /*  MockMvc 테스트 방법
+        1) MockMvc 생성 ( 빈 준비)
+        2) MockMvc 에게 요청에 대한 정보를 입력
+        3) 요청에 대한 응답값을 expect를 이용해서 테스트를 한다.
+        4) expect 다 통과 하면 테스트 통과
+    * 
+    * */
+    private  final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+    @DisplayName("[api] 게시글 리스트 전체 조회")
+    @Test
+    void articles() throws  Exception{
+//        컨트롤 스페이스바 ;' 알트앤터
+//        MockMvcRequestBuilders.get("/api/articles/")
+//        mvc.perform(get("/api/articles")).andExpect(status().isOk()).andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+
+
+    }
+}
