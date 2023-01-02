e467a05d696875f8505ea4ed48b9065ef571632d
song0412 <sognrok1@gmail.com>
2023-01-02 21:00:52 +0900

    .

--- src/main/java/com/bitstudy/app/repository/ArticleRepository.java
+++ src/main/java/com/bitstudy/app/repository/ArticleRepository.java
@@ -1,13 +1,33 @@
 package com.bitstudy.app.repository;
 
 import com.bitstudy.app.domain.Article;
+import com.bitstudy.app.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /*TDD를 위해서 임시로 만들어놓은 저장소 (이걸로 DB 접근)
  * 할일 - 클래스 레벨에 @RepositoryRestResource 넣어서 해당 클래스를 spring rest
  * */
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article,Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>
+        ,QuerydslBinderCustomizer<QArticle> {
 
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.createAt, root.createBy, root.hashtag);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createBy).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+
+    }
 }
