0b298a2e1e81f978858b66cfb4916bb34e6672ab
song0412 <sognrok1@gmail.com>
2023-01-04 19:23:50 +0900

    #22 - 타임리프 컴포넌트 분리
    기존의 index 파일에 있던 header와 footer를 별도의 파일(컴포넌트)로 분리함.
    * th:replace 적용

--- src/main/java/com/bitstudy/app/controller/Ex08_1_ArticleController_게시판_뷰_비어있는거.java
+++ src/main/java/com/bitstudy/app/controller/Ex08_1_ArticleController_게시판_뷰_비어있는거.java
@@ -2,6 +2,6 @@ package com.bitstudy.app.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/articles")
+@RequestMapping("/articles8")
 public class Ex08_1_ArticleController_게시판_뷰_비어있는거 {
 }
