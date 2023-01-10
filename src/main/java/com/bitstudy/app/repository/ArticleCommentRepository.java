a6c1203ca9f93e6eb5ff35586aa3681a50c10db1
song0412 <sognrok1@gmail.com>
2023-01-10 19:30:15 +0900

    #22 - 비지니스 로직(service) 구현
    테스트를 토대로 게시판 서비스 필요한 비지니스 로직 구현

--- src/main/java/com/bitstudy/app/repository/ArticleCommentRepository.java
+++ src/main/java/com/bitstudy/app/repository/ArticleCommentRepository.java
@@ -1,34 +1,53 @@
 package com.bitstudy.app.repository;
 
-import com.bitstudy.app.domain.Article;
 import com.bitstudy.app.domain.ArticleComment;
-import com.bitstudy.app.domain.QArticle;
 import com.bitstudy.app.domain.QArticleComment;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
-/*TDD를 위해서 임시로 만들어놓은 저장소 (이걸로 DB 접근)
- * 할일 - 클래스 레벨에 @RepositoryRestResource 넣어서 해당 클래스를 spring rest
- * */
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/** QueryDsl 의 QuerydlsPredicateExecutor 와 QuerysalBinberCustomizer 를 이용해서 검색 기능을 만들어볼거다. */
+
 @RepositoryRestResource
 public interface ArticleCommentRepository extends
         JpaRepository<ArticleComment, Long>,
-        QuerydslPredicateExecutor<ArticleComment>,
-        QuerydslBinderCustomizer<QArticleComment> {
+        QuerydslPredicateExecutor<ArticleComment>
+        , QuerydslBinderCustomizer<QArticleComment> {
+
+    /* 게시글에 딸려있는 댓글 검색 */
+    List<ArticleComment> findByArticle_Id(Long articleId);
+    /* 중요!!
+     * findByArticle_Id 해설: 아직은 자동완성 기능 안되지만 일단 findById 같은건데 _(언더바) 의 의미가 별도 있다. 무작정 붙이는거 아님.
+     * 언더바는 타고 들어가야 할때 사용된다.
+
+     * 게시글로 댓그을 검색해야 하는데, 이런 경우에 사용하는 방법이다.
+     * ArticleComment 안에는 Article 이랑 UserAccount 가 있는데, 그 안에 있는 객체 이름인 article을 쓰고 _(언더바)로 내려가면 그 객체 안으로 들어간다.
+
+     * */
+
 
     @Override
     default void customize(QuerydslBindings bindings, QArticleComment root) {
+
         bindings.excludeUnlistedProperties(true);
-        bindings.including( root.content, root.createAt, root.createBy);
+
+        bindings.including(root.content, root.createAt, root.createBy);
+
         bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.createAt).first(DateTimeExpression::eq);
         bindings.bind(root.createBy).first(StringExpression::containsIgnoreCase);
-
     }
+
+    /*  1) 빌드 (ctrl + F9)
+        2) Hal 가서 체크하기
+            ex) http://localhost:8080/api/articleComments?createdBy=Klaus
+                http://localhost:8080/api/articles?createdBy=Klaus
+
+    * */
 }
--- src/main/java/com/bitstudy/app/repository/ArticleRepository.java
+++ src/main/java/com/bitstudy/app/repository/ArticleRepository.java
@@ -4,30 +4,73 @@ import com.bitstudy.app.domain.Article;
 import com.bitstudy.app.domain.QArticle;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-/*TDD를 위해서 임시로 만들어놓은 저장소 (이걸로 DB 접근)
- * 할일 - 클래스 레벨에 @RepositoryRestResource 넣어서 해당 클래스를 spring rest
- * */
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/** QueryDsl 의 QuerydlsPredicateExecutor 와 QuerysalBinberCustomizer 를 이용해서 검색 기능을 만들어볼거다. */
+
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
         QuerydslPredicateExecutor<Article>
         ,QuerydslBinderCustomizer<QArticle> {
+    /* 조심할것: QuerydslBinderCustomizer 는 QArticle을 사용하는데 이건 build.gradle 에서 queryDsl 을 build 하고 와야함 */
+
+    /* 설명: QuerydslPredicateExecutor 는 Article 안에 있는 모든 필드에 대한 기본 검색기능을 추가해준다.
+            순서: 1. 바인딩
+                 2. 검색용 필드를 추가
+    *  */
+
+    /* 제목으로 검색할때 */
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+
+    /* 내용으로 검색할때 */
+    Page<Article> findByContentContaining(String title, Pageable pageable);
+
+    /* 유저아이디로 검색할때 */
+    Page<Article> findByUserAccount_UserIdContaining(String title, Pageable pageable);
+
+    /* 닉네임으로 검색할때 */
+    Page<Article> findByUserAccount_NicknameContaining(String title, Pageable pageable);
+
+    /* 해시태그로 검색할때 */
+    Page<Article> findByhashtagContaining(String title, Pageable pageable);
+
 
     @Override
-    default void customize(QuerydslBindings bindings, QArticle root){
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        /* 1. 바인딩
+            현재 QuerydslPredicateExecutor 때문에 Article 에 있는 모든 필드에 대한 검색이 열려있는 상태이다. 근데 우리가 원하는건 선택적 필드(제목, 본문, id, 글쓴이, 해시태그)만 검색에 사용되도록 하고 싶다.
+            그래서 선택적으로 검색을 하게 하기 위해서 bindings.excludeUnlistedProperties 를 쓴다.
+            excludeUnlistedProperties 는 리스팅을 하지 않은 프로퍼티는 검색에 포함할지 말지를 결정할 수 있는 메서드이다.
+            true 면 검색에서 제외, false 는 모든 프로퍼티를 열어주는거
+        *  */
         bindings.excludeUnlistedProperties(true);
+
+
+        /* 2. 검색용(원하는) 필드를 지정(추가) 하는 부분
+            including 을 이용해서 title, content, createdBy, createdAt, hashtag 검색 가능하게 만들거임.
+            (id 는 인증기능 달아서 유저 정보를 알아 올 수 있을때 할거임)
+            including 사용법: 'root.필드명'
+        * */
         bindings.including(root.title, root.content, root.createAt, root.createBy, root.hashtag);
-        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+
+        /* 3. '정확환 검색'만 됐었는데 'or 검색' 가능하게 바꾸기  */
+        // bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${문자열}' 로 들어감. % 없이 들어가는거라서 수동으로 넣어줘야 함.
+
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${문자열}%'
         bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
-        bindings.bind(root.createAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createAt).first(DateTimeExpression::eq); // 이건 날짜니까 DateTimeExpression 으로 하고, eq는 equals의 의미. 날짜필드는 정확한 검색만 되도록 설정. 근데 이렇게 하면 시분초가 다 0으로 인식됨. 이부분은 별도로 시간 처리할때 건드릴거임.
         bindings.bind(root.createBy).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
 
     }
+
+    /* ArticleCommentRepository.java 가서 똑같이 하기 */
+
 }
